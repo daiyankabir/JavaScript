e96b9a4e746e80571ee42f09d96b318c3403b686
daiyankabir <kabirdaiyan@gmail.com>
2025-02-07 22:08:18 +0100

    fg

---
+++ first.js
@@ -0,0 +1,10 @@
+const age = 18;
+var isAllowed = true;
+
+if (age >= 18) {
+    isAllowed = true;
+    console.log("Access granted");
+} else {
+    isAllowed = false;
+    console.log("Access denied");
+}
