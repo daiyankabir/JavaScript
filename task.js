da058d46c26a1066e64257845abcda44f8ed6167
daiyankabir <kabirdaiyan@gmail.com>
2025-02-16 21:16:19 +0100

    update

--- task.js
+++ task.js
@@ -7,15 +7,31 @@
 //     console.log('Coke: 30tk');
 // }
 
-const weight = 70; // kg
-let height = 5.8; // feet
-height = height / 3.28; // meters = 3.28 feet
-const bmi = weight / (height * height);
+// const weight = 70; // kg
+// let height = 5.8; // feet
+// height = height / 3.28; // meters = 3.28 feet
+// const bmi = weight / (height * height);
 
-if (bmi < 18.5) { console.log('Underweight'); }
+// if (bmi < 18.5) { console.log('Underweight'); }
 
-else if (bmi >= 18.5 && bmi < 25) { console.log('Normal weight'); }
+// else if (bmi >= 18.5 && bmi < 25) { console.log('Normal weight'); }
 
-else if (bmi >= 25 && bmi < 30) { console.log('Overweight'); }
+// else if (bmi >= 25 && bmi < 30) { console.log('Overweight'); }
 
-else { console.log('Obese'); }
+// else { console.log('Obese'); }
+
+
+//Bus Ticket Price
+const age = 18;
+const profession = 'student';
+
+if (age < 10) { console.log('Ticket Free'); }
+
+else if (age >= 10 && age < 60) {
+    if (profession === 'student') { console.log('50% discount'); }
+    else { console.log('Regular ticket fare 800 tk'); }
+}
+
+else if (age >= 60) { console.log('20% discount'); }
+
+else { console.log('Regular ticket fare 800 tk'); }
