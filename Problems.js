b8b811203a33259dc817e61b6436c9252c783d8f
daiyankabir <kabirdaiyan@gmail.com>
2025-02-18 13:34:49 +0100

    update

---
+++ Problems.js
@@ -0,0 +1,26 @@
+//Problem 1
+function cashOut(money) {
+    // You have to write your code here
+}
+
+//problem 2
+function validEmail(email) {
+    // You have to write your code here
+}
+
+//problem 3
+function electionResult(votes) {
+    // You have to write your code here
+}
+
+//problem 4
+function isBestFriend(f1, f2) {
+    // You have to write your code here
+}
+
+
+
+//problem 5
+function calculateWatchTime(times) {
+    // You have to write your code here
+}
