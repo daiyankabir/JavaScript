ac477380f42366f784ee58d6700f31de0ac52b4e
daiyankabir <kabirdaiyan@gmail.com>
2025-02-19 19:09:16 +0100

    update

--- task.js
+++ task.js
@@ -1,54 +1,54 @@
-// const burgerprice = 500;
+const burgerprice = 500;
 
-// if (burgerprice >= 500) {
-//     console.log('Free coke');
-// }
-// else {
-//     console.log('Coke: 30tk');
-// }
+if (burgerprice >= 500) {
+    console.log('Free coke');
+}
+else {
+    console.log('Coke: 30tk');
+}
 
-// const weight = 70; // kg
-// let height = 5.8; // feet
-// height = height / 3.28; // meters = 3.28 feet
-// const bmi = weight / (height * height);
+const weight = 70; // kg
+let height = 5.8; // feet
+height = height / 3.28; // meters = 3.28 feet
+const bmi = weight / (height * height);
 
-// if (bmi < 18.5) { console.log('Underweight'); }
+if (bmi < 18.5) { console.log('Underweight'); }
 
-// else if (bmi >= 18.5 && bmi < 25) { console.log('Normal weight'); }
+else if (bmi >= 18.5 && bmi < 25) { console.log('Normal weight'); }
 
-// else if (bmi >= 25 && bmi < 30) { console.log('Overweight'); }
+else if (bmi >= 25 && bmi < 30) { console.log('Overweight'); }
 
-// else { console.log('Obese'); }
+else { console.log('Obese'); }
 
 
 //Bus Ticket Price
-// const age = 18;
-// const profession = 'student';
+const age = 18;
+const profession = 'student';
 
-// if (age < 10) { console.log('Ticket Free'); }
+if (age < 10) { console.log('Ticket Free'); }
 
-// else if (age >= 10 && age < 60) {
-//     if (profession === 'student') { console.log('50% discount'); }
-//     else { console.log('Regular ticket fare 800 tk'); }
-// }
+else if (age >= 10 && age < 60) {
+    if (profession === 'student') { console.log('50% discount'); }
+    else { console.log('Regular ticket fare 800 tk'); }
+}
 
-// else if (age >= 60) { console.log('20% discount'); }
+else if (age >= 60) { console.log('20% discount'); }
 
-// else { console.log('Regular ticket fare 800 tk'); }
+else { console.log('Regular ticket fare 800 tk'); }
 
 //Concat two arrays
 
-// const firstArray = [1, 2, 3, 4, 5];
+const firstArray = [1, 2, 3, 4, 5];
 
-// const secondArray = [6, 7, 8, 9, 10];
+const secondArray = [6, 7, 8, 9, 10];
 
 // const combinedArray = [...firstArray, ...secondArray];
 // const combinedArray = firstArray.concat(secondArray);
 
-// console.log(combinedArray);
-// const firstStr = 'Hello';
-// const secondStr = 'World';
+console.log(combinedArray);
+const firstStr = 'Hello';
+const secondStr = 'World';
 
-// const combinedStr = firstStr + ' ' + secondStr;
+const combinedStr = firstStr + ' ' + secondStr;
 
-// console.log(combinedStr);
+console.log(combinedStr);
